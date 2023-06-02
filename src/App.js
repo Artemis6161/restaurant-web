a30c51e2983263cc1910a8e74c07e020325d8814
sangeetha <Artemis6161@github.com>
2023-06-02 12:25:07 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,70 @@
 import React from 'react'
+import Home from "./component/home"
+import About from "./component/about"
+import Menu from "./component/menu"
+import Gallery from "./component/gallery"
+import Contact from "./component/contact"
+import Signup from "./component/signup"
+import "../src/App.css"
+import {
+  createBrowserRouter,
+ 
+  RouterProvider,
+ 
+} from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      hello sangeetha
+    <div className='app'>
+     
+     
+      <RouterProvider basename="/home" router={router} />
     </div>
   )
 }
-
-export default App
+const router = createBrowserRouter([
+  {
+     path: "/home",
+    element:  
+   <Home/>
+  
+   
+  },
+  {
+    path: "/about",
+    element:  
+   <About/>
+   
+  },
+  {
+    path: "/about",
+    element:  
+   <Menu/>
+   
+  },
+  {
+    path: "/about",
+    element:  
+   <Gallery/>
+   
+  },
+  {
+    path: "/service",
+    element:  
+   <Service/>
+   
+  },
+  {
+    path: "/contact",
+    element:  
+   <Contact/>
+   
+  },
+  {
+    path: "/signup",
+    element:  
+   <Signup/>
+   
+  },
+]);
+export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div>
+      home
+      about
+     Gallery
+     signup
+    </div>
+  )
+}
+
+export default navbar
